94a304620746f4675435ebe5341f9fa841e052c0
王琛 <18845891861@163.com>
2017-08-19 20:29:51 +0800

    The basic functions have been realized.2

--- 游戏克隆/js/app.js
+++ 游戏克隆/js/app.js
@@ -1,9 +1,10 @@
-
+//常量申明
+var ENEMYnumber= 6,ENEMYx=-30,ENNEMYy=70,ROW=101,COL=83;var count=0;
 // 这是我们的玩家要躲避的敌人
-var Enemy = function(x,y,speed) {
-    this.x=x;
+var Enemy = function(y,speed) {
+    this.x=ENEMYx*Math.floor(Math.random()*10*speed);
     this.y=y;
-    this.speed=speed;
+    this.speed=Math.ceil(Math.random()*10*speed+120);
     // 要应用到每个敌人的实例的变量写在这里
     // 我们已经提供了一个来帮助你实现更多
     // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
@@ -38,18 +39,16 @@ var Player=function(x,y){
     this.sprite='images/char-boy.png';
 };
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
-
-var count=0;
 Player.prototype.update=function(dt){
     if(this.y === -13){
     count++;
     if(count==66){
-        this.x = 202;
-        this.y = 83*4+70;
+        this.x = ROW*2;
+        this.y = COL*4+ENNEMYy;
     }
         console.log(count);
     }
-    if(this.x == 202&&this.y == 83*4+70){
+    if(this.x == ROW*2&&this.y == COL*4+ENNEMYy){
         count=0;
     }
     //console.log(this.x,this.y)
@@ -61,12 +60,12 @@ Player.prototype.handleInput=function(movement){
         switch (movement){
             case'left':
                 if(this.x>0){
-                    this.x -=101 ;
+                    this.x -=ROW ;
                 }break;
 
             case'right':
-                if(this.x<404){
-                    this.x +=101 ;
+                if(this.x<ROW*4){
+                    this.x +=ROW ;
                 }break;
 
             case'up':
@@ -74,13 +73,13 @@ Player.prototype.handleInput=function(movement){
                     if(this.y==-95){
                         this.y=320;
                     }else{
-                        this.y -=83;
+                        this.y -=COL;
                     }
                 }break;
 
             case'down':
                 if(this.y<355){
-                    this.y +=83;
+                    this.y +=COL;
                 }break;
 
         }
@@ -92,8 +91,8 @@ Player.prototype.checkCollisions= function(){
         if(this.y==allEnemies[i].y){
             if(Math.abs(this.x-allEnemies[i].x)<40){
 
-                this.x = 202;
-                this.y = 83*4+70;
+                this.x = ROW*2;
+                this.y = COL*4+ENNEMYy;
             }
         }
     }
@@ -101,14 +100,13 @@ Player.prototype.checkCollisions= function(){
 };
 
 // 现在实例化你的所有对象
-var allEnemies=[
-        new Enemy(22, 83 * 0 + 70, 30) , new Enemy(21, 83 * 0 + 70, 23), // row 1
-        new Enemy(57, 83 * 1 + 70, 29), new Enemy(20, 83 * 1 + 70, 29), // row 2
-        new Enemy(22, 83 * 2 +70, 50), new Enemy(59, 83 * 2 + 70, 50),  // row 3
-        new Enemy(22, 83 * 3 +70, 60), new Enemy(59, 83 * 3 + 70, 20)  // row 4
-];
- // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
-var player=new Player(202, 83*4+70);// 把玩家对象放进一个叫 player 的变量里面
+var allEnemies=[];
+for(var i=0;i<ENEMYnumber;i++){
+   var a= new Enemy(COL* Math.floor(Math.random()*4)+ ENNEMYy, i);
+    allEnemies.push(a);
+}
+// 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
+var player=new Player(ROW*2, COL*4+ENNEMYy);// 把玩家对象放进一个叫 player 的变量里面
 
 //83 * 3+55
 
